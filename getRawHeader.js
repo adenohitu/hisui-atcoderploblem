16b69ad9975c9c01d9cb88979f3da6032c7d65ac
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2022-03-04 13:09:46 +0900

    feat:initPlugin

---
+++ getRawHeader.js
@@ -0,0 +1,5 @@
+const { getRawHeader } = require("asar");
+console.log(getRawHeader("./dist/hisui-atcoderploblem.asar"));
+// console.log(
+//   JSON.stringify(getRawHeader("./dist/hisui-atcoderploblem.asar"), null, "\t")
+// );
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
-
-console.log(hello("World"));
+module.exports = function (name: string): void {
+  console.log(`hello${name}`);
+};
